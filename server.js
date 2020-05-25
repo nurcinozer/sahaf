5855e2e8131e16965b3b3112bb0fb55379c9fc5b
nurçin özer <nurcinpiksel@outlook.com>
2020-05-25 23:28:46 +0300

    initial author routes

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').parse()
+    require('dotenv').config()
 };
 
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
@@ -24,5 +28,6 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log('connected to mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
